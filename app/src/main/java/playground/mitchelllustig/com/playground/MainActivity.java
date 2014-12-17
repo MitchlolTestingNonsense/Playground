26de2c8c9980709778a925f57a2b21373fc9ec56
Mitch Lustig <moesk8er2@gmail.com>
2014-12-17 14:47:43 -0500

    Just a commit to trigger a CI build

--- app/src/main/java/playground/mitchelllustig/com/playground/MainActivity.java
+++ app/src/main/java/playground/mitchelllustig/com/playground/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int x = useLibAdd(5,5);
     }
 
     public int useLibAdd(int a, int b){
